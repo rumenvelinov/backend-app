d915addc600f33b1d98a144d72d11c57e98413d5
Rumen <rumen.velinov@gmail.com>
2023-10-07 07:49:00 +0300

    Adding hello world app

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # backend-app
+
+Simple hello world app using Python.
+
+## How to use it?
+
+To interact with the app, at the command prompt type:
+`python3 hello.py say-hi`
--- hello.py
+++ hello.py
@@ -1,1 +1,17 @@
-print("Hi friends")
+import click
+
+
+@click.group()
+def main():
+    pass
+
+
+@main.command()
+@click.option("--a", prompt="Hi friend. What's your name? ", type=str)
+def say_hi(a):
+    value = a
+    click.echo(" Nice to meet you, {}".format(value))
+
+
+if __name__ == "__main__":
+    main()
